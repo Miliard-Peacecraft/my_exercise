3c58ec86b817b20fdd2f219db387bec2b39c6899
Miliard-Peacecraft <yesyes78920xx@yahoo.com.tw>
2018-08-31 15:13:25 +0800

    增加Remove的宣告

--- my_exercise/AVLTree.h
+++ my_exercise/AVLTree.h
@@ -8,12 +8,16 @@ public:
 	int balance;
 };
 
-class AVLTree :	public BinarySerachTree {
+class AVLTree : public BinarySerachTree {
 public:
 	bool Insert(const int value);
+	bool Remove(const int value);
 
 private:
 	bool _Insert(Node* &node, const int value, bool &updateBalance);
+	bool _Remove(Node* &node, const int value, bool &updateBalance);
+	bool _RemoveLeft(Node* &node, const int value, bool &updateBalance);
+	bool _RemoveRight(Node* &node, const int value, bool &updateBalance);
 	void _RotateLeft(Node* &node);
 	void _RotateRight(Node* &node);
 };
